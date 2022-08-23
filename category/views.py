1d4d421a18e2fc3461b331db5748543fb72ca21c
co11ection <tima.j.zh@gmail.com>
2022-08-23 20:43:13 +0600

    beginning

---
+++ category/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import permissions
+from . import serializers
+from .models import Category
+# Create your views here.
+
+
+class Category(ModelViewSet):
+    queryset = Category.objects.all()
+    permission_classes = (permissions.IsAdminUser)
+
+    
